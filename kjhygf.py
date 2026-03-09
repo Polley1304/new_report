9a49f40f62eae3b35e16bd610a04d1f068607d77
Moses <thinkingbigceo@gmail.com>
2026-03-09 12:18:26 +0530

    my first commit

---
+++ kjhygf.py
@@ -0,0 +1,22 @@
+try:
+    num1 = int(input("Enter your first number "))
+    num2 = int(input("Enter your second number "))
+    operator =input("[*, +, /, - ] ")
+except ValueError:
+     print("you have entered an invalid input")
+else:
+    if operator == "*":
+        print("Result is", num1 * num2)
+
+    elif operator == "+":
+        print("Result is", num1 + num2)
+    elif operator == "-":
+        print("Result is", num1 - num2)
+    elif operator == "/":
+        print("Result is", num1 / num2)
+    else:
+        print("you have enter incorrect operator")
+
+finally:
+    print("Done")
+
